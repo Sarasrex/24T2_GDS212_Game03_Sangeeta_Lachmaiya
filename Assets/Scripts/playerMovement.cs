4f98115476b525f8dfd2e5edeb09fe9c36ce947b
SLachmaiyaSAE <1031886@student.sae.edu.au>
2024-02-27 10:25:18 +1100

    Added buttons and updated build settings

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -10,7 +10,11 @@ public class playerMovement : MonoBehaviour
     public float sidewaysForce = 500f;
 
 
-    // Update is called once per frame
+    private RectTransform leftButton;
+    private RectTransform rightButton;
+
+
+
     void Update()
     {
         rb.AddForce(0, 0, forwardForce * Time.deltaTime);
@@ -23,6 +27,32 @@ public class playerMovement : MonoBehaviour
         if (Input.GetKey("a"))
         {
             rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+    }
+
+
+    private void FixedUpdate()
+    {
+        // Below from Chat gpt with slight mods to fit my current script
+        for (int i = 0; i < Input.touchCount; i++)
+        {
+            Touch touch = Input.GetTouch(i);
+
+            // Check if the touch is within the left button bounds
+            if (RectTransformUtility.RectangleContainsScreenPoint(leftButton, touch.position))
+            {
+                // Move the object to the left
+                transform.Translate(Vector3.left * sidewaysForce * Time.deltaTime);
+            }
+
+            // Check if the touch is within the right button bounds
+            else if (RectTransformUtility.RectangleContainsScreenPoint(rightButton, touch.position))
+            {
+                // Move the object to the right
+                transform.Translate(Vector3.right * sidewaysForce * Time.deltaTime);
+            }
+
         }
 
     }
