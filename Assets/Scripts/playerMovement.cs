08d7dccabc81e9f7762b503fb4444e8c4573fb69
Sangeeta Lachmaiya <1031886@student.sae.edu.au>
2024-02-27 14:49:32 +1100

    Assign button movement

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,53 +8,67 @@ public class playerMovement : MonoBehaviour
     public Rigidbody rb;
 
     public float forwardForce = 2000f;
-    public float sidewaysForce = 500f;
+    public float sidewaysForce = 1000f;
 
-
-    private RectTransform leftButton;
-    private RectTransform rightButton;
-
-
+    public GameObject leftButton;
+    public GameObject rightButton;
 
     void Update()
     {
         rb.AddForce(0, 0, forwardForce * Time.deltaTime);
 
-        if (Input.GetKey("d"))
+        if (Input.GetMouseButtonDown(0))
         {
-            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+            // Left button
+            MoveLeft();
         }
 
-        if (Input.GetKey("a"))
+        if (Input.GetMouseButtonDown(0))
         {
-            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+            // Right button
+            MoveRight();
         }
-
     }
 
-
-    private void FixedUpdate()
+   public void MoveLeft()
     {
-        // Below from Chat gpt with slight mods to fit my current script
-        for (int i = 0; i < Input.touchCount; i++)
-        {
-            Touch touch = Input.GetTouch(i);
+        //Debug.Log("MovingLeft");
+        rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+    }
 
-            // Check if the touch is within the left button bounds
-            if (RectTransformUtility.RectangleContainsScreenPoint(leftButton, touch.position))
-            {
-                // Move the object to the left
-                transform.Translate(Vector3.left * sidewaysForce * Time.deltaTime);
-            }
-
-            // Check if the touch is within the right button bounds
-            else if (RectTransformUtility.RectangleContainsScreenPoint(rightButton, touch.position))
-            {
-                // Move the object to the right
-                transform.Translate(Vector3.right * sidewaysForce * Time.deltaTime);
-            }
-
-        }
-
+    public void MoveRight()
+    {
+        rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
     }
 }
+
+
+
+
+    //private void FixedUpdate()
+    //{
+    //    // Below from Chat gpt with slight mods to fit my current script
+    //    for (int i = 0; i < Input.touchCount; i++)
+    //    {
+    //        Touch touch = Input.GetTouch(i);
+
+    //        // Check if the touch is within the left button bounds
+    //        if (RectTransformUtility.RectangleContainsScreenPoint(leftButton, touch.position))
+    //        {
+    //            // Move the object to the left
+
+    //            transform.Translate(Vector3.left * sidewaysForce * Time.deltaTime);
+                
+    //        }
+
+    //        // Check if the touch is within the right button bounds
+    //        else if (RectTransformUtility.RectangleContainsScreenPoint(rightButton, touch.position))
+    //        {
+    //            // Move the object to the right
+    //            transform.Translate(Vector3.right * sidewaysForce * Time.deltaTime);
+    //        }
+
+    //    }
+
+    //}
+
