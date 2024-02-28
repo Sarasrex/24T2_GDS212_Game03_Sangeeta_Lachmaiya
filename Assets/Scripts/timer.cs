b00abde6944bf3eedd4109ff1c073bacf8ff1ba1
SLachmaiyaSAE <1031886@student.sae.edu.au>
2024-02-29 00:26:56 +1100

    Levels, panels, scene manager, Buttons, timer
    Still need to add Audio and collider script for next level / panel set active features. Still need to make each level different.

---
+++ Assets/Scripts/timer.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+
+    [SerializeField] TextMeshProUGUI timerText;
+    [SerializeField] float remainingTime;
+
+
+    public void Update()
+    {
+        
+        if (remainingTime > 0)
+        {
+            remainingTime -= Time.deltaTime;
+        }
+        else if (remainingTime < 0)
+        {
+            remainingTime = 0;
+            
+            // Audio end noise (win / lose) 
+            // Game over gameObject panel active
+            
+        }
+        
+
+        int minutes = Mathf.FloorToInt(remainingTime / 60);
+        int seconds = Mathf.FloorToInt(remainingTime % 60);
+
+        timerText.text = string.Format("{0}", seconds);
+    }
+}
